1f5e078a26f170768ab7f8649cb2b2226283088a
mr-president-01 <volodymyrherych22@gmail.com>
2019-02-10 23:40:59 +0200

    initial

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 document.querySelector('.page-loaded')
-    .innerText = (new Date()).toLocaleTimeString();
-
+  .innerText = (new Date()).toLocaleTimeString();
+  
 document.querySelector('.ajax-get-html')
-    .addEventListener('click', ajaxGetHtml);
-
+  .addEventListener('click', ajaxGetHtml);
+  
 function ajaxGetHtml(){
     const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.redyState === 4 && xhr.status === 200){
-            document.querySelector('.html-container').innerHTML = xhr.responseText;
-
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector('.html-container')
+              .innerHTML = xhr.responseText;
         }
     }
     xhr.open('GET', 'client-data.html', true);
