485c56fadc4cc966899d5272a7e3a7753e007b58
mr-president-01 <volodymyrherych22@gmail.com>
2019-02-11 15:17:10 +0200

    fetch_php

--- js/index.js
+++ js/index.js
@@ -60,3 +60,16 @@ function fetchGetHtml (){
         .then(html => document.querySelector('.html-container')
                .innerHTML = html);
 }
+
+ document.querySelector('.login-form').addEventListener('submit', submitForm);
+
+ function submitForm(e){
+     e.preventDefault();
+     fetch('form.php', {
+         method: 'POST',
+         body: new FormData(document.querySelector('.login-form'))
+     })
+         .then( response => response.text())
+         .then( html => document.querySelector('.server-response')
+                                        .innerHTML = html);
+ }
