6c34170999b0f225d2c5c419350080f27695c38d
mr-president-01 <volodymyrherych22@gmail.com>
2019-02-11 15:28:12 +0200

    errorfix

--- form.php
+++ form.php
@@ -1,4 +1,4 @@
 <p>
-    Welcome <?php echo $_POST["user-name"]; ?><br>
+    Welcome, <?php echo $_POST["name"]; ?><br>
     Your password is: <?php echo $_POST["password"]; ?>
 </p>
--- js/index.js
+++ js/index.js
@@ -63,13 +63,13 @@ function fetchGetHtml (){
 
  document.querySelector('.login-form').addEventListener('submit', submitForm);
 
- function submitForm(e){
-     e.preventDefault();
-     fetch('form.php', {
-         method: 'POST',
-         body: new FormData(document.querySelector('.login-form'))
-     })
-         .then( response => response.text())
-         .then( html => document.querySelector('.server-response')
-                                        .innerHTML = html);
- }
+function submitForm(e) {
+    e.preventDefault();
+    fetch('form.php', {
+        method: 'POST',
+        body: new FormData(document.querySelector('.login-form'))
+    })
+        .then( response => response.text() )
+        .then( html => document.querySelector('.server-response')
+                                .innerHTML = html );
+}
